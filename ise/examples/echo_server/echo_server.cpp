f8b4e44ffb5dd7352e9210f00d853956c38dc3ae
mfc10001 <yubo.li@PC-91.baoluo.com>
2015-07-10 09:50:45 +0800

    ll

--- ise/examples/echo_server/echo_server.cpp
+++ ise/examples/echo_server/echo_server.cpp
@@ -94,6 +94,30 @@ void AppBusiness::onTcpRecvComplete(const TcpConnectionPtr& connection, void *pa
 {
     logger().writeStr("onTcpRecvComplete");
 
+
+{
+	std::string strValue = "{\"name\":\"json\",\"array\":[{\"cpp\":\"jsoncpp\"},{\"java\":\"jsoninjava\"},{\"php\":\"support\"}]}";
+
+	Json::Reader reader;
+	Json::Value value;
+
+	if (reader.parse(strValue, value))
+	{
+		std::string out = value["name"].asString();
+		std::cout << out << std::endl;
+		const Json::Value arrayObj = value["array"];
+		for (unsigned int i = 0; i < arrayObj.size(); i++)
+		{
+			if (!arrayObj[i].isMember("cpp")) 
+				continue;
+			out = arrayObj[i]["cpp"].asString();
+			std::cout << out;
+			if (i != (arrayObj.size() - 1))
+				std::cout << std::endl;
+		}
+	}
+
+}
     string msg((char*)packetBuffer, packetSize);
 
 	Json::Reader reader;
