bb7554965920dc7ea323a5a28656edc6f395ab56
mfc10001 <yubo.li@PC-91.baoluo.com>
2015-07-16 16:20:31 +0800

    ll

--- ise/build/linux/debug/examples/cmake_install.cmake
+++ ise/build/linux/debug/examples/cmake_install.cmake
@@ -36,6 +36,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ll/baoluo/ise/build/linux/debug/examples/db_server/cmake_install.cmake")
   include("/home/ll/baoluo/ise/build/linux/debug/examples/game_server/cmake_install.cmake")
+  include("/home/ll/baoluo/ise/build/linux/debug/examples/login_server/cmake_install.cmake")
 
 endif()
 
